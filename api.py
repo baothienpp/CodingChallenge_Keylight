5bef387caa44500a2e52d4194433e4908ac3fbeb
Thien <baothien@gmail.com>
2018-11-06 20:21:52 +0100

    Add API end points

--- api.py
+++ api.py
@@ -1,23 +1,66 @@
 from flask import Flask
+import datetime
 import json
 import requests
-#
-# app = Flask(__name__)
-#
-#
-# @app.route("/")
-# def hello():
-#     return "Hello World!"
-#
-# https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc
 
-# Here we print out the retrieved page's HTML to the console
-# once we've got this we can start performing some analysis of
-# the webpage and do some cooler things.
+app = Flask(__name__)
 
-link = 'https://api.github.com/search/repositories?q=created:2018-11-01+stars:>=100&sort=stars&order=desc'
-data = requests.get(link).text
-output = json.loads(data)
-test = output.get('items')
-for key, value in output.get('items'):
-    print(output)
+link = 'https://api.github.com/search/repositories?q=created:%date%star&sort=stars&order=desc'
+
+
+@app.route("/", methods=['GET'])
+def get_total():
+    stars, total = get_stars_watcher()
+    return "Found: %d Repositories. Total of : %d stars " % (total, stars)
+
+
+@app.route("/size/<int:max_size>", methods=['GET'])
+def filter_max_size(max_size):
+    stars, total = get_stars_watcher(size=max_size)
+    return "Found: %d Repositories. Total of : %d stars " % (total, stars)
+
+
+@app.route("/star/<int:star>", methods=['GET'])
+def filter_star(star):
+    stars, total = get_stars_watcher(star=star)
+    return "Found: %d Repositories. Total of : %d stars " % (total, stars)
+
+
+@app.route("/size/<int:max_size>/star/<int:star>", methods=['GET'])
+def filter_multi_parameter(max_size, star):
+    stars, total = get_stars_watcher(size=max_size, star=star)
+    return "Found: %d Repositories. Total of : %d stars " % (total, stars)
+
+
+@app.route("/star/<int:star>/size/<int:max_size>", methods=['GET'])
+def filter_multi_parameter_switch(max_size, star):
+    stars, total = get_stars_watcher(size=max_size, star=star)
+    return "Found: %d Repositories. Total of : %d stars " % (total, stars)
+
+
+def get_stars_watcher(size=None, star=None):
+    link_tmp = link
+    one_week_date = datetime.datetime.now() - datetime.timedelta(days=7)
+    one_week_date = str(one_week_date.date())
+    link_tmp = link_tmp.replace("%date", one_week_date)
+
+    if star:
+        link_tmp = link_tmp.replace("%star", "+stars:>=" + str(star))
+    else:
+        link_tmp = link_tmp.replace("%star", "+stars:>=100")
+
+    data = requests.get(link_tmp).text
+    output = json.loads(data)
+    item_list = output.get('items')
+    if size:
+        item_list = item_list[:size]
+
+    stars = 0
+    total = len(item_list)
+    for item in item_list:
+        stars += item.get('stargazers_count')
+
+    return stars, total
+
+if __name__ == '__main__':
+    app.run(debug=True)
